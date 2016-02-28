f9eb00df6d2273c4076a3bf5cc9f081a5af94bf9
Peihong <peihongguo@gmail.com>
2016-02-28 23:27:05 +0000

    2-42

--- chapter2/2-42.hs
+++ chapter2/2-42.hs
@@ -9,21 +9,23 @@ accumulate_n op initial seqs
 
 flat_map proc s = accumulate (++) [] (map proc s)
 
-safe :: Int -> [(Int, Int)] -> Bool
-safe k positions = accumulate (\p res -> (is_safe p) && res) True $ tail positions
+safe :: [(Int, Int)] -> Bool
+safe positions = accumulate (\p res -> (is_safe p) && res) True $ tail positions
     where is_safe p = (fst p /= row0) && (abs(col0 - (snd p)) /= abs(row0 - (fst p)))
           row0 = fst (head positions)
           col0 = snd (head positions)
 
-adjoin_position :: Int -> Int -> [(Int, Int)] -> [(Int, Int)]
-adjoin_position new_row k rest_of_queens
-        | null rest_of_queens = [(new_row, k)]
-        | otherwise = rest_of_queens ++ [(new_row, k)]
+adjoin_position :: [(Int, Int)] -> Int -> Int -> [(Int, Int)]
+adjoin_position rest_of_queens col new_row
+        | null rest_of_queens = [(new_row, col)]
+        | otherwise = rest_of_queens ++ [(new_row, col)]
 
 queens board_size = queens_col board_size
     where queens_col k
             | k == 0 = [[]]
-            | otherwise = filter (\positions -> safe k positions) $ flat_map (\rest_of_queens -> map (\new_row -> adjoin_position new_row k rest_of_queens) [1..board_size]) $ queens_col (k-1)
+            | otherwise = filter safe $ flat_map (propose_new_configuration k) $ queens_col (k-1)
+                where propose_new_configuration k rest_of_queens = map (adjoin_position rest_of_queens k) [1..board_size]
 
 main = do
-    print $ queens 3
+    n <- getLine
+    mapM_ print $ queens (read n::Int)
