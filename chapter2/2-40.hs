a180e9331f788cacc27d5554520be9690fa83a0b
Peihong <peihongguo@gmail.com>
2016-02-28 20:58:41 +0000

    2-40

--- chapter2/2-40.hs
+++ chapter2/2-40.hs
@@ -18,8 +18,11 @@ prime_sum p = is_prime $ ((p !! 0) + (p !! 1))
 make_pair_sum :: [Int] -> ((Int, Int), Int)
 make_pair_sum p = ((p!!0, p!!1), ((p !! 0) + (p !! 1)))
 
+unique_pairs :: Int -> [[Int]]
+unique_pairs n = flat_map (\i -> map (\j -> [i, j]) [1..(i-1)]) [1..n] 
+
 prime_sum_pairs :: Int -> [((Int, Int), Int)]
-prime_sum_pairs n = map make_pair_sum $ filter prime_sum $ flat_map (\i -> map (\j -> [i, j]) [1..(i-1)]) [1..n]
+prime_sum_pairs n = map make_pair_sum $ filter prime_sum $ unique_pairs n
 
 remove :: Int -> [Int] -> [Int]
 remove x s = filter (\y -> y /= x) s
@@ -30,5 +33,6 @@ permutations s
     | otherwise = flat_map (\x -> map (\p -> [x] ++ p) $ permutations (remove x s)) s
 
 main = do
+    print $ unique_pairs 10
     print $ prime_sum_pairs 10
     print $ permutations [1..5]
